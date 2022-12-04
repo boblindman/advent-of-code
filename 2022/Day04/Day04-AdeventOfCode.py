67302629758bb89cabce316f98a2659767073351
Bob Lindman <bob@lindman.us>
2022-12-04 09:41:12 -0500

    Completed part A

--- 2022/Day04/Day04-AdeventOfCode.py
+++ 2022/Day04/Day04-AdeventOfCode.py
@@ -4,13 +4,26 @@ import os
 import sys
 from aocd import submit 
 scriptdir = os.path.dirname(os.path.abspath(__file__))
-inputFile = "Day03-input.txt"
+inputFile = "Day04-input.txt"
 
 # Set to true when ready to submit the answer
 myPart = "a" # "a" or "b"
 myYear = 2022
-myDay = 0
-imReady = False
+myDay = 4
+imReady = True
+
+def fullyInRange(range1,range2):
+    range1End = range1.start
+    if len(range1) > 1:
+        range1End = range1[-1]
+    range1InRange2 = range1.start in range2 and range1End in range2
+    print("{} {} {}".format(range1,range1.start,range1End))
+    range2End = range2.start
+    if len(range2) > 1:
+        range2End = range2[-1]
+    range2InRange1 = range2.start in range1 and range2End in range1
+    print("{} {} {} {}".format(range2,range2.start,range2End, range2End in range1))
+    return range1InRange2 or range2InRange1
 
 if __name__ == "__main__":
     myAnswer = 0
@@ -18,7 +31,14 @@ if __name__ == "__main__":
     Lines = file1.readlines()
     for line in Lines:
         myData = line.strip()
-        print("{}".format(myData))
+        elf1,elf2 = myData.split(",")
+        elf1RangeStart,elf1RangeEnd = (elf1.split("-"))
+        elf2RangeStart,elf2RangeEnd = (elf2.split("-"))
+        elf1Range = range(int(elf1RangeStart),int(elf1RangeEnd)+1)
+        elf2Range = range(int(elf2RangeStart),int(elf2RangeEnd)+1)
+        if fullyInRange(elf1Range,elf2Range):
+            myAnswer+= 1
+        print("{}:{}  {}".format(elf1Range,elf2Range,myAnswer))
 
     print(myAnswer)
 
